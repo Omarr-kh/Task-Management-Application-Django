69d0bd9c0d2ca84d971f2908b5adb86b5bb6d64b
Omar <omarkhaled1752003@yahoo.com>
2024-08-25 16:24:55 +0300

    added documentation

--- tasks/views.py
+++ tasks/views.py
@@ -8,21 +8,32 @@ from django.core.paginator import Paginator
 
 
 def home(request):
+    """ View to show all tasks using pagination (5 per page) """
+
     user = User.objects.last()
     tasks = Task.objects.filter(user=user)
+
+    # pagination with maximum of 5 tasks per page
     paginator = Paginator(tasks, 5)
+
     # Get the page number from the query string
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
         "tasks": page_obj
     }
+
+    # render a certain page
     return render(request, 'home.html', context)
 
 
 def add_task(request):
+    """ View to add a new task to the database """
+
     if request.method == "POST":
         user = User.objects.last()
+
+        # retrieve task fields from the form inputs
         title = request.POST.get('title')
         description = request.POST.get('description')
         is_completed = request.POST.get('is_completed') == 'on'
@@ -39,6 +50,8 @@ def add_task(request):
 
 
 def view_task(request, task_id):
+    """ View to show a specific task based on it's id """
+
     task = Task.objects.get(id=task_id)
     context = {
         "task": task
@@ -47,14 +60,15 @@ def view_task(request, task_id):
 
 
 def update_task(request, task_id):
-    # retrieve the task by id
+    """ View to update a specific task based on it's id """
+
     task = Task.objects.get(id=task_id)
     context = {
         "task": task
     }
 
     if request.method == "POST":
-        # update the task and save it
+        # update the task fields from the form inputs and save it
         task.title = request.POST.get('title')
         task.description = request.POST.get('description')
         task.status = request.POST.get('is_completed') == 'on'
@@ -65,6 +79,8 @@ def update_task(request, task_id):
 
 
 def delete_task(request, task_id):
+    """ View to delete a specific task based on it's id """
+
     task = Task.objects.get(id=task_id)
     task.delete()
 
