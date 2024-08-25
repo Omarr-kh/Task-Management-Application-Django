057c43c5ddf93e15595df4787385bc590f418e0a
Omar <omarkhaled1752003@yahoo.com>
2024-08-25 13:45:27 +0300

    pass tasks to the home template

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    tasks = Task.objects.all()
+    context = {
+        "tasks": tasks
+    }
+    return render(request, 'home.html', context)
