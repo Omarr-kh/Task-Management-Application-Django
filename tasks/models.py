9afe28aeb51274210947f106b9a4f3249ec8a3d3
Omar <omarkhaled1752003@yahoo.com>
2024-08-25 16:27:15 +0300

    added documentation

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Task(models.Model):
+    # each task has to be linked with a user
+    # all tasks are deleted if the user is deleted
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+# urls for Tasks application
 urlpatterns = [
     path('', views.home, name='home'),
     path('add-task/', views.add_task, name='add-task'),
