9abf7edaacf46c8cb360c3757135fb0d1c8ea582
Omar <omarkhaled1752003@yahoo.com>
2024-08-25 13:49:52 +0300

    return tasks based on the user

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
-    tasks = Task.objects.all()
+    user = User.objects.last()
+    tasks = Task.objects.filter(user=user)
     context = {
         "tasks": tasks
     }
