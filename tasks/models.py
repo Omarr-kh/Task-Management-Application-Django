df0683c36b03905ad2b71f9079fd6dc887d7c423
Omar <omarkhaled1752003@yahoo.com>
2024-08-25 13:41:40 +0300

    added verbose_name to status

--- tasks/models.py
+++ tasks/models.py
@@ -5,4 +5,4 @@ class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
-    status = models.BooleanField(default=False)
+    status = models.BooleanField(default=False, verbose_name='completed')
