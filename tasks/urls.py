0e256ce362cfc6f5c782a1d62eae922c446c8e7e
Omar <omarkhaled1752003@yahoo.com>
2024-08-25 14:36:52 +0300

    added urls for viewing and deleting a task

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('add-task/', views.add_task, name='add-task')
+    path('add-task/', views.add_task, name='add-task'),
+    path('view-task/<int:task_id>/', views.view_task, name='view-task'),
+    path('delete-task/<int:task_id>/',
+         views.delete_task, name='delete-task'),
 ]
